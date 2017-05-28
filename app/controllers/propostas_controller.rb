6106c9b7fcab59a96a5bac7dec68ce41b06c2e42
Gabriel N. Alves <gabriel.neubaner.alves@gmail.com>
2017-05-28 15:41:22 -0300

    Fields format

--- app/controllers/propostas_controller.rb
+++ app/controllers/propostas_controller.rb
@@ -29,7 +29,7 @@ class PropostasController < ApplicationController
 
     respond_to do |format|
       if @proposta.save
-        format.html { redirect_to @proposta, notice: 'Proposta was successfully created.' }
+        format.html { redirect_to @proposta, notice: 'Proposta criada.' }
         format.json { render :show, status: :created, location: @proposta }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class PropostasController < ApplicationController
   def update
     respond_to do |format|
       if @proposta.update(proposta_params)
-        format.html { redirect_to @proposta, notice: 'Proposta was successfully updated.' }
+        format.html { redirect_to @proposta, notice: 'Proposta atualizada.' }
         format.json { render :show, status: :ok, location: @proposta }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class PropostasController < ApplicationController
   def destroy
     @proposta.destroy
     respond_to do |format|
-      format.html { redirect_to propostas_url, notice: 'Proposta was successfully destroyed.' }
+      format.html { redirect_to propostas_url, notice: 'Proposta removida.' }
       format.json { head :no_content }
     end
   end
